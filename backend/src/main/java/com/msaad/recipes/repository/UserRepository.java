2a76666dd0a31dba3d152213c9eb25d4ca2926b6
Mohammed Al-Khafaji <mo.saad.najim@gmail.com>
2025-12-11 14:15:18 +0100

    Add UserRepository

--- backend/src/main/java/com/msaad/recipes/repository/UserRepository.java
+++ backend/src/main/java/com/msaad/recipes/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.msaad.recipes.repository;
 
-public interface UserRepository {
+import com.msaad.recipes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
