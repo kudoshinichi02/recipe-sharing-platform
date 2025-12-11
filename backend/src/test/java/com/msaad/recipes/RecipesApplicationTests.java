9897d4f1b24383dfd375db9a6d99eef89aa67fda
Mohammed Al-Khafaji <mo.saad.najim@gmail.com>
2025-12-11 13:32:23 +0100

    Add Spring Boot backend initial project

---
+++ backend/src/test/java/com/msaad/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msaad.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
