0a45e6ebf7aba8479e2c5cc090e300235b56b000
Mohammed Al-Khafaji <mo.saad.najim@gmail.com>
2026-03-09 16:52:13 +0100

    Add Axios interceptor to attach Basic Auth credentials automatically

--- frontend/recipe-frontend/src/api/axios.js
+++ frontend/recipe-frontend/src/api/axios.js
@@ -1,10 +1,29 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  // Do NOT attach auth when registering
+  if (config.url === "/auth/register") {
+    return config;
+  }
+
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+
+  if (username && password) {
+    config.auth = {
+      username: username,
+      password: password,
+    };
+  }
+
+  return config;
 });
 
 export default api;
